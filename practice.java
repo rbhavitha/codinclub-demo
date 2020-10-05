2ecce4f87bd55220cf7f2a91b90001394766733d
rbhavitha <bhavitharedrouthu@gmail.com>
2020-10-05 12:44:30 +0530

    sum of two numbers

---
+++ practice.java
@@ -0,0 +1,11 @@
+
+public class practice {
+  public static void main(String[] args) {
+    int x = 5;
+    int y = 6;
+    int sum = x + y;
+    System.out.println("Sum of two numbers is:"+sum); 
+  }
+}
+
+
